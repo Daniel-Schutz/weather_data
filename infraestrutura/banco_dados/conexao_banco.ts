30337f7b50ff3c85bd867a4b9426bdb578b77a92
Daniel Schutz <109694403+Daniel-Schutz@users.noreply.github.com>
2024-04-28 15:33:23 -0400

    final version

--- infraestrutura/banco_dados/conexao_banco.ts
+++ infraestrutura/banco_dados/conexao_banco.ts
@@ -5,4 +5,13 @@ const sequelize = new Sequelize('database', 'username', 'password', {
   dialect: 'postgres',
 });
 
+// Testar a conexão com o banco de dados
+sequelize.authenticate()
+  .then(() => {
+    console.log('Conexão com o banco de dados estabelecida com sucesso.');
+  })
+  .catch((error) => {
+    console.error('Erro ao conectar-se ao banco de dados:', error);
+  });
+
 export default sequelize;
