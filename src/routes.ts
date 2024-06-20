b7dcc1fbdd92d991dc56489401b2dd957b08745f
Pedrao1234 <pedrosotoma@gmail.com>
2024-06-20 08:47:05 -0400

    refactor: adaptando código para serviço de dados climáticos apenas

--- src/routes.ts
+++ src/routes.ts
@@ -1,18 +1,12 @@
 import { Router } from 'express';
 import DadosClimaticosController from './interfaces/controllers/DadosClimaticosController';
-import NotificacaoController from './interfaces/controllers/NotificacaoController';
 
 const router = Router();
 const dadosClimaticosController = new DadosClimaticosController();
-const notificacaoController = new NotificacaoController();
 
 // Rotas para dados climaticos
 router.post('/coletar-dados', dadosClimaticosController.coletarDados);
 router.get('/listar-dados', dadosClimaticosController.listarDados);
 
-// Rotas para notificações
-router.post('/cadastrar', notificacaoController.cadastrar);
-router.post('/descadrastar', notificacaoController.descadastrar);
-router.get('/inscritos', notificacaoController.listarCadastrados);
 
 export default router;
